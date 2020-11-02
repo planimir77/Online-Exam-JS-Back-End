981fe86a9f1c5618df0d1710ced172334aa768e7
planimir77 <planimir7708@gmail.com>
2020-11-02 11:46:49 +0200

    Initial commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,6 +5,7 @@ const { authCookieName, authHeaderName, jwtSecret } = config;
 module.exports = function (req, res, next) {
   const token = req.cookies[authCookieName] || req.headers[authHeaderName];
   if (!token) { next(); return; }
+  
   jwt.verify(token, jwtSecret, function (err, decoded) {
     if (err) { next(err); return; }
     req.user = { _id: decoded.userId, };
