5ad39373a5543fb7b328a06254736ef3166363f2
planimir77 <planimir7708@gmail.com>
2020-11-02 11:56:08 +0200

    Fix\ logged user home page data

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,6 @@
 const { getCourses } = require('./course')
+const homePageTitle = "JS Back-End - Exam - November 2020";
+
 module.exports = {
     get: {
         async homePage(req, res) {
@@ -6,19 +8,25 @@ module.exports = {
             try {
                 
                 if (res.locals.isLogged) {
-                    const courses = await getCourses(query);
-                    return res.render('user/home', { pagetitle: "JS Back-End - Exam - November 2020", courses: courses, });
+                    // all public courses sorted by the created time in ascending order
+                    const courses = await (await getCourses({ isPublic: true, })).sort((a, b) => {
+                        return a.created - b.created;
+                    });
+
+                    return res.render('user/home', { pageTitle: homePageTitle, courses: courses, });
                 }
 
-                const courses = await (await getCourses()).sort((a, b) => {
+                // 3 public courses sorted by the count of enrolled in users in descending order
+                const courses = await (await getCourses({ isPublic: true, })).sort((a, b) => {
                     return b.users.length - a.users.length;
-                }).slice(0,3);
+                })
+                .slice(0,3);
 
-                res.render('guest/home', { pagetitle: "JS Back-End - Exam - November 2020", topcourses: courses, });
+                res.render('guest/home', { pageTitle: homePageTitle, topcourses: courses, });
 
             } catch (error) {
                 console.error('Error :', error);
-                res.render('guest/home', { pagetitle: "JS Back-End - Exam - November 2020", errorMessage: error.message, });
+                res.render('guest/home', { pageTitle: homePageTitle, errorMessage: error.message, });
             }
         },
     },
